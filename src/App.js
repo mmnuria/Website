e507be40dc8257525b48ae4d9e9af2c7416f3b40
Sergio Gutierrez Villalba <gv.sergiodevs@gmail.com>
2023-08-12 16:03:13 +0200

    in progress

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import github from './gh.png';
 import linkedin from './li.png';
 import gmail from './gm.png';
 import './App.css';
-import { useEffect } from 'react';
 
 const links = {
   github: 'https://github.com/mmnuria',
@@ -11,7 +10,7 @@ const links = {
 }
 
 const summary = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam bibendum, ipsum at tempus consequat, est dui lobortis urna, vel ultricies metus libero ac diam. Curabitur consequat efficitur mollis. Pellentesque bibendum orci dui, in posuere nulla finibus sit amet. In venenatis tincidunt tortor, sed facilisis mauris efficitur nec.';
-
+//const summary_2 = Hola <br> Adios <br> Buenas <br> Hasta luego;
 function App() {
   function onClickImage(type) {
     window.location.href = links[type];
@@ -20,22 +19,27 @@ function App() {
   return (
     <div className='main theme-background'>
       <div className='menu'>
-        <img onClick={() => onClickImage('github')} src={github} className="menu-icon" alt="github" />
-        <img onClick={() => onClickImage('linkedin')} src={linkedin} className="menu-icon" alt="linkedin" />
-        <img onClick={() => onClickImage('gmail')} src={gmail} className="menu-icon" alt="gmail" />
+        <img onClick={() => onClickImage('github')} src={github} className="menu-icon item" alt="github" />
+        <img onClick={() => onClickImage('linkedin')} src={linkedin} className="menu-icon item" alt="linkedin" />
+        <img onClick={() => onClickImage('gmail')} src={gmail} className="menu-icon item" alt="gmail" />
       </div>
 
       <div className='outer-body'>
         <div className='body'>
           <div className='border'>
-            <div className='title theme-headline'>Soy el bichoflor.</div>
+            <div className='title theme-headline'>Welcome.</div>
             <div className='summary theme-paragraph'> {summary} </div>
           </div>
-          <div className='border'>
-            Conocimientos
+          <div className='right-section'>
+            <div className='theme-headline subtitle'><center>Lenguajes</center></div>
+            <div className='summary theme-paragraph'> <center><p>C++</p>  <p>Java</p> <p>JavaScript</p></center> </div>
           </div>
         </div>
       </div>
+
+      <footer className='footer theme-paragraph item'>
+      mmnuria - 2023
+      </footer>
 
     </div>
   );
